789cf94f80945b1321a33380c8d5ca2b701063d2
jason-xt <649223152@qq.com>
2018-04-23 11:51:49 +0800

    Update AliOssAdapter.php

--- src/AliOssAdapter.php
+++ src/AliOssAdapter.php
@@ -566,7 +566,7 @@ class AliOssAdapter extends AbstractAdapter
      */
     public function getUrl( $path )
     {
-        if (!$this->has($path)) throw new FileNotFoundException($filePath.' not found');
+        //if (!$this->has($path)) throw new FileNotFoundException($filePath.' not found');
         return ( $this->ssl ? 'https://' : 'http://' ) . ( $this->isCname ? ( $this->cdnDomain == '' ? $this->endPoint : $this->cdnDomain ) : $this->bucket . '.' . $this->endPoint ) . '/' . ltrim($path, '/');
     }
 
