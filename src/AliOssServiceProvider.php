1e424a1616c3b6e62f037885ae83ae253f770b5b
qihao <qihao@shopex.cn>
2018-07-22 19:33:23 +0800

    增加使用签名URL进行临时授权

--- src/AliOssServiceProvider.php
+++ src/AliOssServiceProvider.php
@@ -4,6 +4,7 @@ namespace Jacobcyl\AliOSS;
 
 use Jacobcyl\AliOSS\Plugins\PutFile;
 use Jacobcyl\AliOSS\Plugins\PutRemoteFile;
+use Jacobcyl\AliOSS\Plugins\SignUrl;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
