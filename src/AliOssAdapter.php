2482d751cbc169034aeda1f6df2901ff3e229155
coden <admitrjuk@findevelop.ru>
2018-08-03 09:07:21 +0700

    ReadStream wrap to stream if string returned

--- src/AliOssAdapter.php
+++ src/AliOssAdapter.php
@@ -449,10 +449,15 @@ class AliOssAdapter extends AbstractAdapter
     public function readStream($path)
     {
         $result = $this->readObject($path);
-        $result['stream'] = $result['raw_contents'];
+        if (is_resource($result['raw_contents'])) {
+            $result['stream'] = $result['raw_contents'];
+            // Ensure the EntityBody object destruction doesn't close the stream
+            $result['raw_contents']->detachStream();
+        } else {
+            $result['stream'] = fopen('php://temp', 'r+');
+            fwrite($result['stream'], $result['raw_contents']);
+        }
         rewind($result['stream']);
-        // Ensure the EntityBody object destruction doesn't close the stream
-        $result['raw_contents']->detachStream();
         unset($result['raw_contents']);
 
         return $result;
